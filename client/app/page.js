992b938edb6050175991aa0324906dd2a5523e37
Bukola Laniyan <105382504+khenzee@users.noreply.github.com>
2026-04-20 12:49:00 +0100

    design system setup

--- client/app/page.js
+++ client/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1 className="font-helixa ">Home is here to stay</h1>
     </main>
   );
 }
